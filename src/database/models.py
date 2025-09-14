3f0bafd9fa7e8920f145091b22f2ab24c0cbdd8b
Borys <boris02374@gmail.com>
2025-09-14 23:41:25 +0300

    added UsersModel

--- src/database/models.py
+++ src/database/models.py
@@ -1,10 +1,21 @@
-from sqlalchemy import Column, Integer, String, DATE, text
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from datetime import date
+from sqlalchemy import Column, Integer, String, DATE, ForeignKey, DateTime, func, Boolean
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from datetime import date, datetime
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(String(50), index=True)
+    email: Mapped[str] = mapped_column(String(50), unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column(String, index=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=func.now())
+    avatar: Mapped[str] = mapped_column(String(255), nullable=True) 
+    confirmed: Mapped[bool] = mapped_column(Boolean, default=False)
 
 
 class ContactsModel(Base):
@@ -16,3 +27,5 @@ class ContactsModel(Base):
     phone_number: Mapped[str] = mapped_column(String(20), unique=True)
     birthday: Mapped[date] = mapped_column(DATE, nullable=False, index=True)
     other_info: Mapped[str] = mapped_column(String(250), nullable=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
+    user: Mapped["User"] = relationship(backref="contacts")
