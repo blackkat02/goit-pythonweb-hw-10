8e6c7114079de66d0e17c1a4a0429ffd9587f6ab
Borys <boris02374@gmail.com>
2025-09-17 23:28:45 +0300

    updated .env

--- src/database/db.py
+++ src/database/db.py
@@ -5,17 +5,17 @@ from sqlalchemy.orm import sessionmaker
 # from jose import jwt
 
 
-# DB_USER = os.getenv("DB_USER")
-# DB_PASS = os.getenv("DB_PASS")
-# DB_NAME = os.getenv("DB_NAME")
-# DB_HOST = os.getenv("DB_HOST")  #, "localhost")
-# DB_PORT = os.getenv("DB_PORT")  #, "5432")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")  #, "localhost")
+DB_PORT = os.getenv("DB_PORT")  #, "5432")
 
-DB_USER = os.getenv("DB_USER", "admin")
-DB_PASS = os.getenv("DB_PASS", "admin")
-DB_NAME = os.getenv("DB_NAME", "postgres_db")
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
+# DB_USER = os.getenv("DB_USER", "admin")
+# DB_PASS = os.getenv("DB_PASS", "admin")
+# DB_NAME = os.getenv("DB_NAME", "postgres_db")
+# DB_HOST = os.getenv("DB_HOST", "localhost")
+# DB_PORT = os.getenv("DB_PORT", "5432")
 
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
